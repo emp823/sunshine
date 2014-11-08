c80c23047d1f5bd652e138dbba6e6f9ea500a92f
Erik Pearson <emp823@gmail.com>
2014-11-08 18:49:49 -0500

    Add update and delete

--- app/src/androidTest/java/com/erikmpearson/sunshine/TestProvider.java
+++ app/src/androidTest/java/com/erikmpearson/sunshine/TestProvider.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
 import com.erikmpearson.sunshine.data.WeatherDbHelper;
 import com.erikmpearson.sunshine.data.WeatherContract.LocationEntry;
@@ -104,8 +105,6 @@ public class TestProvider extends AndroidTestCase {
         TestDb.validateCursor(weatherCursor, weatherValues);
 
 
-        // Add the location values in with the weather data so that we can make
-        // sure that the join worked and we actually get all the values back
         addAllContentValues(weatherValues, testValues);
 
         // Get the joined Weather and Location data
@@ -161,9 +160,142 @@ public class TestProvider extends AndroidTestCase {
         assertEquals(LocationEntry.CONTENT_ITEM_TYPE, type);
     }
 
+    public void testUpdateLocation() {
+        // Create a new map of values, where column names are the keys
+        ContentValues values = TestDb.createNorthPoleLocationValues();
+
+        Uri locationUri = mContext.getContentResolver().
+                insert(LocationEntry.CONTENT_URI, values);
+        long locationRowId = ContentUris.parseId(locationUri);
+
+        assertTrue(locationRowId != -1);
+        Log.d(LOG_TAG, "New row id: " + locationRowId);
+
+        ContentValues updatedValues = new ContentValues(values);
+        updatedValues.put(LocationEntry._ID, locationRowId);
+        updatedValues.put(LocationEntry.COLUMN_CITY_NAME, "Santa's Village");
+
+        int count = mContext.getContentResolver().update(
+                LocationEntry.CONTENT_URI, updatedValues, LocationEntry._ID + "= ?",
+                new String[] { Long.toString(locationRowId)});
+
+        assertEquals(count, 1);
+
+        Cursor cursor = mContext.getContentResolver().query(
+                LocationEntry.buildLocationUri(locationRowId),
+                null,
+                null, // Columns for the "where" clause
+                null, // Values for the "where" clause
+                null // sort order
+        );
+
+        TestDb.validateCursor(cursor, updatedValues);
+    }
+
+    // Make sure we can still delete after adding/updating stuff
+    public void testDeleteRecordsAtEnd() {
+        deleteAllRecords();
+    }
+
     void addAllContentValues(ContentValues destination, ContentValues source) {
         for (String key : source.keySet()) {
             destination.put(key, source.getAsString(key));
         }
+    }
+
+    static final String KALAMAZOO_LOCATION_SETTING = "kalamazoo";
+    static final String KALAMAZOO_WEATHER_START_DATE = "20140625";
+
+    long locationRowId;
+
+    static ContentValues createKalamazooWeatherValues(long locationRowId) {
+        ContentValues weatherValues = new ContentValues();
+        weatherValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+        weatherValues.put(WeatherEntry.COLUMN_DATETEXT, KALAMAZOO_WEATHER_START_DATE);
+        weatherValues.put(WeatherEntry.COLUMN_DEGREES, 1.2);
+        weatherValues.put(WeatherEntry.COLUMN_HUMIDITY, 1.5);
+        weatherValues.put(WeatherEntry.COLUMN_PRESSURE, 1.1);
+        weatherValues.put(WeatherEntry.COLUMN_MAX_TEMP, 85);
+        weatherValues.put(WeatherEntry.COLUMN_MIN_TEMP, 35);
+        weatherValues.put(WeatherEntry.COLUMN_SHORT_DESC, "Cats and Dogs");
+        weatherValues.put(WeatherEntry.COLUMN_WIND_SPEED, 3.4);
+        weatherValues.put(WeatherEntry.COLUMN_WEATHER_ID, 42);
+
+        return weatherValues;
+    }
+
+    static ContentValues createKalamazooLocationValues() {
+        // Create a new map of values, where column names are the keys
+        ContentValues testValues = new ContentValues();
+        testValues.put(LocationEntry.COLUMN_LOCATION_SETTING, KALAMAZOO_LOCATION_SETTING);
+        testValues.put(LocationEntry.COLUMN_CITY_NAME, "Kalamazoo");
+        testValues.put(LocationEntry.COLUMN_COORD_LAT, 42.2917);
+        testValues.put(LocationEntry.COLUMN_COORD_LONG, -85.5872);
+
+        return testValues;
+    }
+
+    // Inserts both the location and weather data for the Kalamazoo data set.
+    public void insertKalamazooData() {
+        ContentValues kalamazooLocationValues = createKalamazooLocationValues();
+        Uri locationInsertUri = mContext.getContentResolver()
+                .insert(LocationEntry.CONTENT_URI, kalamazooLocationValues);
+        assertTrue(locationInsertUri != null);
+
+        locationRowId = ContentUris.parseId(locationInsertUri);
+
+        ContentValues kalamazooWeatherValues = createKalamazooWeatherValues(locationRowId);
+        Uri weatherInsertUri = mContext.getContentResolver()
+                .insert(WeatherEntry.CONTENT_URI, kalamazooWeatherValues);
+        assertTrue(weatherInsertUri != null);
+    }
+
+    public void testUpdateAndReadWeather() {
+        insertKalamazooData();
+        String newDescription = "Cats and Frogs (don't warn the tadpoles!)";
+
+        // Make an update to one value.
+        ContentValues kalamazooUpdate = new ContentValues();
+        kalamazooUpdate.put(WeatherEntry.COLUMN_SHORT_DESC, newDescription);
+
+        mContext.getContentResolver().update(
+                WeatherEntry.CONTENT_URI, kalamazooUpdate, null, null);
+
+        Cursor weatherCursor = mContext.getContentResolver().query(
+                WeatherEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+        );
+
+        // Make the same update to the full ContentValues for comparison.
+        ContentValues kalamazooAltered = createKalamazooWeatherValues(locationRowId);
+        kalamazooAltered.put(WeatherEntry.COLUMN_SHORT_DESC, newDescription);
+
+        TestDb.validateCursor(weatherCursor, kalamazooAltered);
+    }
+
+    public void testRemoveHumidityAndReadWeather() {
+        insertKalamazooData();
+
+        mContext.getContentResolver().delete(WeatherEntry.CONTENT_URI,
+                WeatherEntry.COLUMN_HUMIDITY + " = " + locationRowId, null);
+
+        Cursor weatherCursor = mContext.getContentResolver().query(
+                WeatherEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+        );
+
+        // Make the same update to the full ContentValues for comparison.
+        ContentValues kalamazooAltered = createKalamazooWeatherValues(locationRowId);
+        kalamazooAltered.remove(WeatherEntry.COLUMN_HUMIDITY);
+
+        TestDb.validateCursor(weatherCursor, kalamazooAltered);
+        int idx = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_HUMIDITY);
+        assertEquals(-1, idx);
     }
 }
